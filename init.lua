0aef0ee3bdfd7803f4907964a9506bb61b0740b6
R3Kr <76439773+R3Kr@users.noreply.github.com>
2025-01-07 09:39:25 +0100

    neotree

--- init.lua
+++ init.lua
@@ -935,6 +935,16 @@ require('lazy').setup({
       -- add any options here
     }
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+  },
 
 
 
