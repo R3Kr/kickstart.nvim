2bf3d89d47805f849023658b0180ee9067265b44
R3Kr <76439773+R3Kr@users.noreply.github.com>
2025-01-07 11:44:34 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -943,7 +943,17 @@ require('lazy').setup({
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
       -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    }
+    },
+    config = function ()
+    require('neo-tree').setup({window = {filesystem= {hijack_netrw_behaviour = "open_default"}}})
+    vim.keymap.set('n','<leader>n', function ()
+      require('neo-tree.command').execute({
+         toggle = true
+        })
+     end, {desc = 'Toggles Neotree'})
+    end
+  },
+
   },
 
 
